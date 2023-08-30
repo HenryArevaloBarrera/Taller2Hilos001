cf27eba0d6db985d95b72f417ce31bb8f64b8bb5
HenryArevaloBarrera <henry.arevalo@uptc.edu.co>
2023-08-29 23:21:54 -0500

    001

--- src/Presentacion/Main.java
+++ src/Presentacion/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("args = " + args);
         System.out.println("args = " + args);
         System.out.println("args = " + args);
+        System.out.println(" = " );
     }
 }
